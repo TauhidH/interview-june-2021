e7de56be9540cc1e33b28fd9c2bf9c6fc1ebb53d
TauhidH <86230042+TauhidH@users.noreply.github.com>
2021-06-22 12:20:43 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,23 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int[] binarynum = new int[32];
+    int i=0;
+    while (n>0)
+    {
+      binaryNum[i]= n%2;
+      n=n/2;
+      i++;
+    }
+   for (int j=1 - 1; j >= 0; j--)
+     System.out.print(binaryNum[j]);
+    
     return "";
   }
+
+public static void main(String[] args)
+{
+  int n = 17;
+  String toBinary(n);
+}
 }
